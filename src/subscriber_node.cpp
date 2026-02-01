e958eb8467abea16300bda5e95bbc52df7e71078
Vijay Kiran Reddy <vijayakkala06@gmail.com>
2026-02-01 20:47:34 +0530

    Remove merge conflict markers from subscriber_node.cpp

--- src/subscriber_node.cpp
+++ src/subscriber_node.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
@@ -29,44 +28,5 @@ int main(int argc, char * argv[])
   rclcpp::spin(std::make_shared<SubscriberNode>());
   rclcpp::shutdown();
   return 0;
-=======
-#include <memory>
-#include <functional>
-
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-
-using std::placeholders::_1;
-
-/*
- * TODO: Create a Class named 'SubscriberNode' that inherits from rclcpp::Node.
- * Requirements:
- * 1. The constructor should name the node "subscriber_node".
- * 2. Create a subscription to topic "/chatter" with message type std_msgs::msg::String.
- * 3. The subscription callback should:
- *    - Log using RCLCPP_INFO: "I heard: 'MESSAGE'" where MESSAGE is the received data
- */
-
-class SubscriberNode : public rclcpp::Node
-{
-public:
-    SubscriberNode()
-        : Node("subscriber_node")
-    {
-        // TODO: Create the subscription here
-    }
-
-private:
-    // TODO: Define the topic_callback function here
-
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-};
-
-int main(int argc, char *argv[])
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<SubscriberNode>());
-    rclcpp::shutdown();
-    return 0;
->>>>>>> 0af5982ac57a4d0c3036fcbcafe23c806a58e3fd
 }
+
